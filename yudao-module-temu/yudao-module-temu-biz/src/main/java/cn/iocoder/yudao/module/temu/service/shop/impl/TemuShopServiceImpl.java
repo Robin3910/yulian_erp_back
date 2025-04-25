8491835ceb4d0ff336f1000dec2602ae5510e8a7
ykh <1315130764@qq.com>
2025-04-25 14:50:04 +0800

    fix(shop): 修复分页查询店铺的pageNo和pageSize为null的问题

--- yudao-module-temu/yudao-module-temu-biz/src/main/java/cn/iocoder/yudao/module/temu/service/shop/impl/TemuShopServiceImpl.java
+++ yudao-module-temu/yudao-module-temu-biz/src/main/java/cn/iocoder/yudao/module/temu/service/shop/impl/TemuShopServiceImpl.java
@@ -12,12 +12,10 @@ import cn.iocoder.yudao.module.temu.dal.mysql.TemuShopMapper;
 import cn.iocoder.yudao.module.temu.service.shop.TemuShopService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.Resource;
-
 import java.util.Objects;
-
 import static cn.iocoder.yudao.framework.common.exception.util.ServiceExceptionUtil.exception;
+import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
 
 @Service
 @Slf4j
@@ -40,11 +38,11 @@ public class TemuShopServiceImpl implements TemuShopService {
         // 返回
         return shop.getId();
     }
-    
+
     @Override
     public void updateShop(TemuShopUpdateReqVO updateReqVO) {
         TemuShopDO temuShopDO = temuShopMapper.selectByShopId(updateReqVO.getShopId());
-        
+
         // 校验存在
         if (temuShopDO!=null&&!Objects.equals(temuShopDO.getId(), updateReqVO.getId())&&!temuShopDO.getDeleted()) {
             throw exception(new ErrorCode(1_002_001_001, "店铺ID重复"));
@@ -53,30 +51,38 @@ public class TemuShopServiceImpl implements TemuShopService {
         TemuShopDO updateObj = BeanUtils.toBean(updateReqVO, TemuShopDO.class);
         temuShopMapper.updateById(updateObj);
     }
-    
+
     @Override
     public void deleteShop(Long id) {
         // 校验存在
         if (temuShopMapper.selectById(id) == null) {
             throw exception(new ErrorCode(1_002_001_001, "店铺不存在"));
         }
-        
+
         // 删除
         temuShopMapper.deleteById(id);
     }
-    
+
     @Override
     public TemuShopRespVO getShop(Long id) {
         TemuShopDO shop = temuShopMapper.selectById(id);
         return BeanUtils.toBean(shop, TemuShopRespVO.class);
     }
-    
+
     @Override
     public PageResult<TemuShopRespVO> getShopPage(TemuShopPageReqVO pageReqVO) {
-        PageResult<TemuShopDO> pageResult = temuShopMapper.selectPage(
-                pageReqVO.getShopId(), pageReqVO.getShopName(), pageReqVO.getCreateTime());
-        return BeanUtils.toBean(pageResult, TemuShopRespVO.class);
+        PageResult<TemuShopDO> pageResult = temuShopMapper.selectPage(pageReqVO, new LambdaQueryWrapperX<TemuShopDO>()
+                .eqIfPresent(TemuShopDO::getShopId, pageReqVO.getShopId())
+                .likeIfPresent(TemuShopDO::getShopName, pageReqVO.getShopName())
+                .betweenIfPresent(TemuShopDO::getCreateTime, pageReqVO.getCreateTime())
+                .orderByDesc(TemuShopDO::getId));
+
+        PageResult<TemuShopRespVO> result = BeanUtils.toBean(pageResult, TemuShopRespVO.class);
+        result.setPageNo(pageReqVO.getPageNo());
+        result.setPageSize(pageReqVO.getPageSize());
+        return result;
     }
+
     
     @Override
     public TemuShopDO getShopByShopId(Long shopId) {
