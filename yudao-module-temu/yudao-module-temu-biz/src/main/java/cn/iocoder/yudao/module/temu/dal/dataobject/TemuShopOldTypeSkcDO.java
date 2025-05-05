224a6aef419148ce55f3064d54854e02404f0b65
ykh <1315130764@qq.com>
2025-05-05 19:14:44 +0800

    feat(shop-oldType): -新增 Temu店铺合规单skc 接口 -增加 批量保存合规单SKC 和 获取合规单信息 功能

---
+++ yudao-module-temu/yudao-module-temu-biz/src/main/java/cn/iocoder/yudao/module/temu/dal/dataobject/TemuShopOldTypeSkcDO.java
@@ -0,0 +1,28 @@
+package cn.iocoder.yudao.module.temu.dal.dataobject;
+
+import cn.iocoder.yudao.framework.mybatis.core.dataobject.BaseDO;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@TableName(value = "temu_shop_old_type_skc", autoResultMap = true)
+public class TemuShopOldTypeSkcDO extends BaseDO {
+
+    @TableId
+    private Long id;
+
+    //店铺ID
+    private Long shopId;
+
+    //SKC编号
+    private String skc;
+
+    //合规单URL
+    private String oldTypeUrl;
+
+    //合规单类型
+    private String oldType;
+}
---
+++ yudao-module-temu/yudao-module-temu-biz/src/main/java/cn/iocoder/yudao/module/temu/dal/mysql/TemuShopOldTypeSkcMapper.java
@@ -0,0 +1,13 @@
+package cn.iocoder.yudao.module.temu.dal.mysql;
+
+import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
+import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.iocoder.yudao.module.temu.dal.dataobject.TemuShopOldTypeSkcDO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface TemuShopOldTypeSkcMapper extends BaseMapperX<TemuShopOldTypeSkcDO> {
+
+
+}
